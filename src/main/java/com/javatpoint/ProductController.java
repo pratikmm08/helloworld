1770dbf21c2902ce5210988530a646b85fd8a461
pratikmm08 <88031654+pratikmm08@users.noreply.github.com>
2021-11-24 15:35:42 +0530

    Update ProductController.java

--- src/main/java/com/javatpoint/ProductController.java
+++ src/main/java/com/javatpoint/ProductController.java
@@ -2,7 +2,10 @@ package com.javatpoint;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class ProductController 
 {
@@ -17,4 +20,14 @@ List<Product> products = productService.findAll();
 //returns the product list
 return products;
 }
+  
+  @PostMapping(path = "/save-cust-info")
+    public String customerInformation(@RequestBody Object cust) {
+
+        /* You can write your DAO logic here.
+         * For time being I am printing the customer data just to show the POST call is working.
+         */
+
+        return "Customer information saved successfully ::." + cust.protoPayload();
+    }
 }
