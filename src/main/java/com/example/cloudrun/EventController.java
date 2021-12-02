ed67d40b42ded8bb89b147ee9018676748ce4e39
pratikmm08 <88031654+pratikmm08@users.noreply.github.com>
2021-12-02 15:42:06 +0530

    Update EventController.java

--- src/main/java/com/example/cloudrun/EventController.java
+++ src/main/java/com/example/cloudrun/EventController.java
@@ -71,7 +71,7 @@ public class EventController {
             //eventarcFileImportService.fileImport(clientId,tableId,bucketName,filePath);
 System.out.println("Event import request Done!"+bucketName+"||"+projectId+"||"+fileName+"||"+filePath+"||"+tableId+"||"+clientId+"||"+fileName);
         }
-        return ResponseEntity.status(304).build();
+        return ResponseEntity.status(202).build();
     }
 }
 // [END eventarc_generic_handler]
